f5674ccd69f1c43210b1688514d9a850f0576003
Ngoc Hoang <ngochoang09121996@gmail.com>
2018-07-26 10:37:09 +0700

    inital

---
+++ djangoTenant/Manager/admin.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.utils.translation import ugettext_lazy as _
+
+from django.contrib import auth
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import *
+# Register your models here.
+
+admin.site.unregister(auth.models.Group)
+
+class UserBaseAdmin(UserAdmin):
+    list_display = ('id','email','first_name','last_name','birthday')
+    ordering = ('-date_joined',)
+    fieldsets = (
+        (None, {'fields': ('username','email', 'password')}),
+        (_('Personal info'),
+            {'fields': ('first_name', 'last_name', 'birthday','avatar',
+            'avatar_url','is_email_verified','fb_uid','opt')}),
+        (_('Permissions'), 
+            {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), 
+            {'fields': ('last_login', 'date_joined')}),
+        )
+
+    readonly_fields = ('fb_uid',)
+admin.site.register(UserBase, UserBaseAdmin)
---
+++ djangoTenant/Manager/models.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import AbstractUser
+from rest_framework.authtoken.models import Token
+from django.db.models.signals import post_save, post_delete, pre_save, pre_delete
+from django.dispatch import receiver
+
+# Create your models here.
+
+class UserBase(AbstractUser):
+    email = models.EmailField(_('email address'), unique=True)
+    birthday = models.DateField(null=True, blank=False)
+    avatar = models.ImageField(help_text=_('Picture shall be squared, max 640*640, 500k'), upload_to='avatars',
+                                 null=True, blank=True)
+    avatar_url = models.CharField(max_length=200, null=True, blank=True)
+    opt = models.CharField(max_length=255, null=True, blank=True)
+    is_email_verified = models.BooleanField(default=False)
+    fb_uid = models.CharField(max_length=200, null=True, blank=True)
+    fb_access_token = models.CharField(max_length=1000, null=True, blank=True)
+
+    @property
+    def full_name(self):
+        return u'{} {}'.format(self.first_name,self.last_name)
+
+    @property
+    def token(self):
+        return self.auth_token.key
+
+    @property
+    def qr_code(self):
+        datetime_format = '%Y-%m-%d %H:%M:%S'
+        # today = (self.last_login).strftime(datetime_format)
+        data = u'User:{} {} Id:{}'.format(self.first_name, self.last_name, self.id)
+        data = urllib.quote_plus(data)
+        return u'https://api.qrserver.com/v1/create-qr-code/?size=300x300&data={}'.format(data)
+
+@receiver(post_save, sender=UserBase)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
+class UserLog(models.Model):
+    user = models.ForeignKey("UserBase", related_name="log_at")
+    creation_date = models.DateTimeField(auto_now_add=True)
